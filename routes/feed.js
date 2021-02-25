05ce64157c002da2617ca297cf0c92a578616ac8
tomaszbabinski <tommybab@gmail.com>
2021-02-25 21:57:45 +0100

    Added new route for post request

--- routes/feed.js
+++ routes/feed.js
@@ -6,4 +6,7 @@ const router = express.Router();
 //GET  /feed/posts
 router.get('/posts',feedController.getPosts);
 
+//POST /feed/post
+router.post('/post',feedController.createPost);
+
 module.exports = router;
