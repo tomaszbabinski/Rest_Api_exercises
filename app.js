d1f33dc1eda49fb00cc66adba398f294a74e4775
tomaszbabinski <tommybab@gmail.com>
2021-02-25 21:58:43 +0100

    Registering bodyParser in app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
+
+app.use(bodyParser.json()); //application/json
+
 //GET  /feed/posts
 app.use('/feed',feedRoutes);
 
