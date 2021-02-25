a8fa218c46c5073880b3c557584942743490dcf9
tomaszbabinski <tommybab@gmail.com>
2021-02-25 21:58:15 +0100

    Adjusting feed controller by adding createPost method

--- controller/feed.js
+++ controller/feed.js
@@ -1,3 +1,19 @@
 exports.getPosts = (req, res, next) => {
-    
+    res.status(200).json({
+        posts: [{title: 'sample data', content: 'Sample content'}]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    //Create post in db
+    res.status(201).json({
+        message: 'Post created successfuly',
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content
+        }
+    });
 };
