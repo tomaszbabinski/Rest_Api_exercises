1a5dbd9959c725beb185116ff92c6d7c9444c5df
tomaszbabinski <tommybab@gmail.com>
2021-02-27 21:58:28 +0100

    CORS headers added

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
 
-app.use(bodyParser.json()); //application/json
+app.use(bodyParser.json()); //application/jsonx
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 //GET  /feed/posts
 app.use('/feed',feedRoutes);
