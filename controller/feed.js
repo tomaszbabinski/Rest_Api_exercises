d1ca293e3b57a184ef2e0d7dc489ec22a53da887
tomaszbabinski <tommybab@gmail.com>
2021-03-04 20:35:40 +0100

    Storing a post in mongodb

--- controller/feed.js
+++ controller/feed.js
@@ -1,5 +1,7 @@
 const {validationResult} = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -28,17 +30,23 @@ exports.createPost = (req, res, next) => {
 
     const title = req.body.title;
     const content = req.body.content;
-    //Create post in db
-    res.status(201).json({
-        message: 'Post created successfuly',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Tom'
-            },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/photo.jpeg',
+        creator: {
+            name: 'Tom'
         }
     });
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Post created successfuly',
+                post: result
+            });
+        })
+        .catch(err => {
+            console.log(err)
+        }) 
 };
