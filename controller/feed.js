c51ba0aa78c4ae704aa04f55c4f48b47058287e6
tomaszbabinski <tommybab@gmail.com>
2021-03-11 21:16:42 +0100

    Pagination logic added

--- controller/feed.js
+++ controller/feed.js
@@ -5,9 +5,22 @@ const {validationResult} = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    Post.find()
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage -1) * perPage)
+                .limit(perPage);
+        })
         .then(posts => {
-            res.status(200).json({message: 'Fetched posts succeeded',posts: posts});
+            res.status(200).json({
+                message: 'Fetched posts succeeded',
+                posts: posts,
+                totalItems: totalItems
+            });
         })
         .catch(err => {
             if(!err.statusCode){
