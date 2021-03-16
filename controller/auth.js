fc7e8b6054c472f3db4a180abaa9610b39706495
tomaszbabinski <tommybab@gmail.com>
2021-03-16 20:42:09 +0100

    Login and creating the JSON web token

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,6 @@
 const { validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -58,7 +59,13 @@ exports.login = (req, res, next) => {
                 error.statusCode = 401;
                 throw error;
             }
-
+            const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            },'superSecretStringToPlaceHere',
+            {expiresIn: '1h'}
+            );
+            res.status(200).json({token: token, userId: loadedUser._id.toString()})
         })
         .catch(error => {
             if(!error.statusCode){
