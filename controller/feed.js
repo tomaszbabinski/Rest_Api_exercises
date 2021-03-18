f1467e5ad3e317ec2094cb906ecf960d8863a791
tomaszbabinski <tommybab@gmail.com>
2021-03-18 21:11:18 +0100

    Linking an user and a post

--- controller/feed.js
+++ controller/feed.js
@@ -3,6 +3,7 @@ const path = require('path')
 const {validationResult} = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -45,20 +46,31 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     const imageUrl = req.file.path.replace("\\","/");
+    let creator;
+
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {
-            name: 'Tom'
-        }
+        creator: req.userId
     });
     post.save()
         .then(result => {
-            console.log(result);
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfuly',
-                post: result
+                post: post,
+                creator: {
+                    _id: creator._id,
+                    name: creator.name
+                }
             });
         })
         .catch(err => {
