193dd227c4c0ae4d91d3486f20a49aa7c6d6ee96
tomaszbabinski <tommybab@gmail.com>
2021-03-21 15:10:49 +0100

    Getting and updating User status

--- controller/auth.js
+++ controller/auth.js
@@ -74,3 +74,50 @@ exports.login = (req, res, next) => {
             next(error);
         })
 }
+
+exports.getUserStatus = (req,res,next) => {
+
+    User.findById(req.userId)
+        .then(user => {
+            if(!user){
+                const error = new Error('User not found');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({
+                message: 'Status found',
+                status: user.status
+            });
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                  err.statusCode = 500;
+              }
+              next(err);
+        })
+}
+
+exports.updateUserStatus = (req, res, next) => {
+    const newStatus = req.body.status;
+    User.findById(req.userId)
+        .then(user => {
+            if(!user){
+                const error = new Error('User not found');
+                error.statusCode = 404;
+                throw error;
+            }
+            user.status = newStatus;
+            return user.save() 
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'status updated'
+            });
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                  err.statusCode = 500;
+              }
+              next(err);
+        })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {body} = require('express-validator')
 const User  = require('../models/user');
+const isAuth = require('../middleware/is-auth');
 const authController = require('../controller/auth');
 const router = express.Router();
 
@@ -27,6 +28,12 @@ router.put('/signup',[
         .isEmpty()
 ], authController.singup);
 
-router.post('/login',authController.login)
+router.post('/login',authController.login);
+
+router.patch('/status',isAuth, [
+    body('status').trim().not().isEmpty()
+],authController.updateUserStatus)
+
+router.get('/status', isAuth, authController.getUserStatus);
 
 module.exports = router;
