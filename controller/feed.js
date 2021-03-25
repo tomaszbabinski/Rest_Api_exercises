bf3b7174c05d19c127784297acc08249384a4f5a
tomaszbabinski <tommybab@gmail.com>
2021-03-25 20:24:42 +0100

    Changing getPost to async await

--- controller/feed.js
+++ controller/feed.js
@@ -70,25 +70,25 @@ exports.createPost = async (req, res, next) => {
     }
 };
 
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post.findById(postId)
-        .then(post => {
-            if(!post){
-                const error = new Error('Could not find post');
-                error.statusCode = 404;
-                throw error;
-            }
-            res.status(200).json({
-                messasge: 'Post fetched.',
-                post: post})
-        })
-        .catch(err => {
-          if(!err.statusCode){
-                err.statusCode = 500;
-            }
-            next(err);
-        });    
+    try {
+        const post = await Post.findById(postId)   
+        if(!post){
+            const error = new Error('Could not find post');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            messasge: 'Post fetched.',
+            post: post
+        });
+    } catch (err) {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }   
 }
 
 exports.updatePost = (req,res,next) => {
