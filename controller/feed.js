e6cc57f0bc136ede5548657ed0fdd76c2b0d021c
tomaszbabinski <tommybab@gmail.com>
2021-03-25 20:18:10 +0100

    Changing create post to async await

--- controller/feed.js
+++ controller/feed.js
@@ -28,7 +28,7 @@ exports.getPosts = async (req, res, next) => {
     }   
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         const error = new Error('Validation failed, entered data not correct');
@@ -43,7 +43,6 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     const imageUrl = req.file.path.replace("\\","/");
-    let creator;
 
     const post = new Post({
         title: title,
@@ -51,23 +50,24 @@ exports.createPost = (req, res, next) => {
         imageUrl: imageUrl,
         creator: req.userId
     });
-    post.save()
-        .then(result => User.findById(req.userId))
-        .then(user => creator = user)
-        .then(result => res.status(201).json({
-                message: 'Post created successfuly',
-                post: post,
-                creator: {
-                    _id: creator._id,
-                    name: creator.name
-                }
-            }))
-        .catch(err => {
-            if(!err.statusCode){
-                err.statusCode = 500;
-            }
-            next(err);
-        }) 
+
+    try {
+        const result = await post.save();
+        const creator = await User.findById(req.userId);      
+        res.status(201).json({
+                    message: 'Post created successfuly',
+                    post: post,
+                    creator: {
+                        _id: creator._id,
+                        name: creator.name
+                    }
+        });
+    } catch (err){
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
 
 exports.getPost = (req, res, next) => {
