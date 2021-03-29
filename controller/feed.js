4c1ff771427be001816661c69795812d9d8904e8
tomaszbabinski <tommybab@gmail.com>
2021-03-29 20:03:30 +0200

    Changing updatePost to async/await

--- controller/feed.js
+++ controller/feed.js
@@ -91,7 +91,7 @@ exports.getPost = async (req, res, next) => {
     }   
 }
 
-exports.updatePost = (req,res,next) => {
+exports.updatePost = async (req,res,next) => {
     const postId = req.params.postId;
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -110,37 +110,36 @@ exports.updatePost = (req,res,next) => {
         error.statusCode = 422;
         throw error;
     }
-
-    Post.findById(postId)
-        .then(post => {
-            if(!post){
-                const error = new Error('Could not find post');
-                error.statusCode = 404;
-                throw error;  
-            }
-            if(post.creator.toString() !== req.userId){
-                const error = new Error('Not authorized');
-                error.statusCode = 403;
-                throw error;
-            }
-            if(imageUrl !== post.imageUrl){
-               clearImage(post.imageUrl); 
-            }
-            post.title = title;
-            post.imageUrl = imageUrl;
-            post.content = content;
-            return post.save();
+    try {
+        const post = await Post.findById(postId);
+    
+        if(!post){
+            const error = new Error('Could not find post');
+            error.statusCode = 404;
+            throw error;  
+        }
+        if(post.creator.toString() !== req.userId){
+            const error = new Error('Not authorized');
+            error.statusCode = 403;
+            throw error;
+        }
+        if(imageUrl !== post.imageUrl){
+            clearImage(post.imageUrl); 
+        }
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+        const result = await post.save();
+        res.status(200).json({
+            message: 'Post updated',
+            post: result
         })
-        .then(result => res.status(200).json({
-                message: 'Post updated',
-                post: result
-            }))
-        .catch(err => {
-            if(!err.statusCode){
-                err.statusCode = 500;
-            }
-            next(err);
-        })
+    } catch (err) {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 }
 
 exports.deletePost = (req,res,next) => {
