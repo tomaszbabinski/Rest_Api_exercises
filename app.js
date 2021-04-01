7c95faabb4a4ff2691259c28acc9b702f3d825f8
tomaszbabinski <tommybab@gmail.com>
2021-04-01 18:02:56 +0200

    Sharing io websocket instance across files

--- app.js
+++ app.js
@@ -56,14 +56,26 @@ app.use((error, req, res, next) => {
 
 })
 
-mongoose.connect(MONGODB_URI)
+// mongoose.connect(MONGODB_URI)
+//         .then(result => {
+//             const server = app.listen(8080);
+//             const io = require('socket.io')(server, {
+//                 cors: {
+//                 origin: "http://localhost:3000",
+//                 methods: ["GET", "POST"]
+//                 }});
+//             io.on('connection',socket => {
+//                 console.log('Client connected');
+//             });
+//         })
+//         .catch(err=> {
+//             console.log(err);
+//         });
+ 
+        mongoose.connect(MONGODB_URI)
         .then(result => {
             const server = app.listen(8080);
-            const io = require('socket.io')(server, {
-                cors: {
-                origin: "http://localhost:3000",
-                methods: ["GET", "POST"]
-                }});
+            const io = require('./socket').init(server);
             io.on('connection',socket => {
                 console.log('Client connected');
             });
@@ -71,4 +83,3 @@ mongoose.connect(MONGODB_URI)
         .catch(err=> {
             console.log(err);
         });
- 
