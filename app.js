d17ae22566623915e0240fdf82ac5945c11c1130
tomaszbabinski <tommybab@gmail.com>
2021-04-22 20:52:31 +0200

    Added sample error handling for wrong input

--- app.js
+++ app.js
@@ -54,6 +54,15 @@ app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    formatError(err) {
+        if(!err.originalError){
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occured'
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data};
+    }
   }));
 
 app.use((error, req, res, next) => {
