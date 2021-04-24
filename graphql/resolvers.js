4fe11c3dfeceffcde910726ffc4c21d33d7218c4
tomaszbabinski <tommybab@gmail.com>
2021-04-24 17:00:53 +0200

    Added resolver for createPost - for the moment on without userData

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const Post = require('../models/post');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
@@ -58,5 +59,35 @@ module.exports = {
         },'somesupersecret', {expiresIn: '1h' }
         );
         return { token: token, userId: user._id.toString()};
+    },
+    createPost: async function({postInput}, req){
+        const errors = [];
+        if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, {min: 5})){
+            errors.push({message: 'Title is invalid'});
+        }
+        if(validator.isEmpty(postInput.content) || !validator.isLength(postInput.content, {min: 5})){
+            errors.push({message: 'Content is invalid'});
+        }
+
+        if(errors.length > 0){
+            const error = new Error('Invalid input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl
+        });
+
+        const createdPost = await post.save();
+        //add post to user posts later on
+        return {
+                ...createdPost._doc, _id: createdPost._id.toString(),
+                createdAt: createdPost.createdAt.toISOString(),
+                updatedAt: createdPost.updatedAt.toISOString()
+            }
     }
 };
